0ddca26409dc7c10f486809ee1ba996eac29ec96
John Paul Dala <jaypee@lingjack.com>
2024-12-21 20:57:38 +0800

    Add a rotating cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import {Canvas, useFrame} from "@react-three/fiber";
+import {OrbitControls, Sparkles} from "@react-three/drei";
+import {useRef} from "react";
+
+const RotatingCube = () => {
+    const meshRef = useRef()
+
+    useFrame(() => {
+        if(meshRef.current){
+            meshRef.current.rotation.y += 0.001
+            meshRef.current.rotation.x += 0.001
+        }
+    })
+    return (
+        <mesh ref={meshRef}>
+            <cylinderGeometry args={[1, 1, 1]} />
+            <meshLambertMaterial color={"#468585"} emissive={"#468585"}/>
+
+            <Sparkles count={100} scale={1} size={5} speed={0.002} noise={0.2} color="red" />
+        </mesh>
+    )
+}
 const App = () => {
     return (
-    <div>
-        Hello Three.js
-    </div>
+    <Canvas style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <OrbitControls enableZoom enablePan enableRotate />
+        <directionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6}/>
+
+        <color attach="background" args={[0xf0f0f0]} />
+
+        <RotatingCube />
+    </Canvas>
 
     );
 }
